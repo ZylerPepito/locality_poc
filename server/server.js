cd42219a03ebf269c64ae78b27704ced62c3836c
ZylerPepito <108784796+ZylerPepito@users.noreply.github.com>
2026-02-12 11:31:59 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,6 +47,6 @@ app.use("/api", routes);
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
