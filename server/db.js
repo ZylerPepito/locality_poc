a94a6d1661af813bcff4ef34273d572656f8d4bf
ZylerPepito <108784796+ZylerPepito@users.noreply.github.com>
2026-02-12 11:08:12 +0800

    adding the railway database

--- server/db.js
+++ server/db.js
@@ -1,4 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection(process.env.MYSQL_URL);
+
+db.connect(err => {
+  if (err) {
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("Connected to Railway MySQL");
+  }
+});
+
+module.exports = db;
+
+/*
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -16,3 +32,5 @@ db.connect(err => {
 });
 
 module.exports = db;
+
+!*/
--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use("/api", routes);
 // Serve frontend
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
